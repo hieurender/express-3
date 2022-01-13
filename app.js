04a133c7359df5493943ebc56057032df2e00136
Hieu <hieu@render.com>
2022-01-12 16:09:13 -0800

    init

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.send("Hello there 2!"));
+app.get("/envs", (req, res) => res.send(`SECRET: ${process.env['SECRET']}; SECRET_2: ${process.env['SECRET_2']}`));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
